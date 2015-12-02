5a8f479829a942a43c417d7178e836c7e6418e20
claudiocavallaro <claudio.cavallaro91@gmail.com>
2015-12-02 12:08:25 +0100

    ...

--- app/src/main/java/com/example/claudiocavallaro/progettogad/persistenza/RestCall.java
+++ app/src/main/java/com/example/claudiocavallaro/progettogad/persistenza/RestCall.java
@@ -4,6 +4,15 @@ import android.os.AsyncTask;
 
 import com.example.claudiocavallaro.progettogad.modello.ListaGestori;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by claudiocavallaro on 27/11/15.
  */
@@ -16,7 +25,19 @@ public class RestCall extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
         try {
-
+            HttpClient request = new DefaultHttpClient();
+            HttpGet get = new HttpGet(url);
+            HttpResponse response = request.execute(get);
+            int responseCode = response.getStatusLine().getStatusCode();
+            if (responseCode == 200) {
+                InputStream istream = response.getEntity().getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(istream));
+                String s = null;
+                StringBuffer sb = new StringBuffer();
+                while ((s = reader.readLine()) != null) {
+                    sb.append(s);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }

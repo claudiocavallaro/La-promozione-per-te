dd267ee5e6fc1245b3ca059211822b9d992257fa
claudiocavallaro <claudio.cavallaro91@gmail.com>
2015-12-04 17:21:11 +0100

    ...

---
+++ app/src/main/java/com/example/claudiocavallaro/progettogad/Activity/CercaActivity.java
@@ -0,0 +1,17 @@
+package com.example.claudiocavallaro.progettogad.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.claudiocavallaro.progettogad.R;
+
+/**
+ * Created by claudiocavallaro on 04/12/15.
+ */
+public class CercaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}

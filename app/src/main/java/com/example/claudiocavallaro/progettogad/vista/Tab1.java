b0c341a8d9ae62cbafb5922caf52ecce822b3467
claudiocavallaro <claudio.cavallaro91@gmail.com>
2015-12-11 21:37:37 +0100

    ...

--- app/src/main/java/com/example/claudiocavallaro/progettogad/vista/Tab1.java
+++ app/src/main/java/com/example/claudiocavallaro/progettogad/vista/Tab1.java
@@ -1,8 +1,6 @@
 package com.example.claudiocavallaro.progettogad.vista;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
